fea0390ac2e08c94735be653b4b53a51cdd52116
Arguggi <git@arguggi.co.uk>
2016-11-01 16:15:09 +0100

    Add indent script

---
+++ hindentall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+projects=("heed-backend" "heed-frontend" "heed-lib")
+
+for i in "${projects[@]}"; do
+    find "$i/src/" -iname "*.hs" -type f \
+        -exec stylish-haskell -i {} \; \
+        -exec hindent --indent-size 4 --line-length 100 --no-force-newline {} \;
+done
