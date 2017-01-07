8909753ba80009358470cbe9bc022beaf7421ab0
Arguggi <git@arguggi.co.uk>
2017-01-07 16:19:24 +0100

    Add echo to install

--- install.sh
+++ install.sh
@@ -8,8 +8,12 @@ if [[ $EUID -ne 0 ]]; then
    exit 100
 fi
 
+echo "Running as root";
+
 # Create heed user
 useradd --home-dir /etc/heed --create-home --shell /bin/false --system heed -p "useless"
+
+echo "Created heed user";
 
 # Remove heed user password and disable login
 passwd -d heed
@@ -18,19 +22,26 @@ usrmod -s /sbin/nologin heed
 cp ./confs/backend.ini /etc/heed/
 
 # Become postgres to create heed user and import table structure
+
+echo "Setting up postgres"
 su - postgres
 sudo -u postgres psql -h localhost -f ./confs/db/user.sql
 sudo -u postgres psql -h localhost -f ./confs/db/tables.sql
 # Add my user, hardcode everything for now
 sudo -u postgres psql -h localhost -f ./confs/db/arguggi.sql
+echo "PostgreSQL setup"
 
 # Build the backend
+echo "Building"
 cabal new-build heed-backend
 
 # Copy backend to to /usr/bin
+echo "Copying binary to /usr/bin/"
 find dist-newstyle -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
 
 # Copy systemd service file
+
+echo "Copying service file to /usr/lib/systemd/system/heed.service"
 cp ./confs/heed.service /usr/lib/systemd/system/heed.service
 
 chown heed:heed /etc/heed/backend.ini
