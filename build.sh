82f1eba3f8ba2706848c319328be615afec6775c
Arguggi <git@arguggi.co.uk>
2017-01-25 20:25:43 +0100

    New build.sh with version

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 # Exit on error
 set -e
 
+VERSION=0.2.0.0;
+
 # Build the backend
 echo "Building"
 cabal new-build heed-backend
 
 # Copy backend to to /usr/bin
 echo "Copying binary to /usr/bin/"
-sudo find dist-newstyle -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
+sudo find dist-newstyle/build/heed-backend-$VERSION/ -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
