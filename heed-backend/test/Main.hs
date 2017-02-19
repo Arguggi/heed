f15aed1cd2f2b2fd9ad59015cc1c7ef6bbf8debf
Arguggi <git@arguggi.co.uk>
2017-02-19 19:09:20 +0100

    Test sql file

--- heed-backend/test/Main.hs
+++ heed-backend/test/Main.hs
@@ -6,11 +6,16 @@ import Control.Exception (bracket)
 import Control.Monad (forM_)
 import Data.ByteString (ByteString)
 import Data.Int (Int64)
-import Database.PostgreSQL.Simple as PSQL
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
+import qualified Database.PostgreSQL.Simple as PG
+import Database.PostgreSQL.Tmp (withTmpDB, DBInfo(..))
 import qualified Heed.QueryTest as QT
 import Heed.Types (execQuery, runTest)
 import qualified Opaleye.Trans as OT
 import Test.Hspec (describe, hspec, it, shouldSatisfy)
+import System.Process (callProcess)
 
 testingDB :: ByteString
 testingDB = "dbname='heed' user='heed'"
@@ -24,10 +29,20 @@ tables =
     , (QT.checkUserTable, "User Tables")
     ]
 
+connect :: ByteString -> (PG.Connection -> IO ()) -> IO ()
+connect connString = bracket (PG.connectPostgreSQL connString) PG.close
+
 main :: IO ()
-main =
-    bracket (PSQL.connectPostgreSQL testingDB) PSQL.close $ \conn ->
-        hspec $
-        describe "SQL tables should match haskell" $
+main = do
+    connect testingDB (runSQLTests "SQL tables should match haskell")
+    withTmpDB $ \(DBInfo tempName tempRole) -> do
+        let tempDB = encodeUtf8 $ "dbname='" <> tempName <> "' user='" <> tempRole <> "'"
+        callProcess "/usr/bin/psql" ["-d", T.unpack tempName, "-U", T.unpack tempRole, "-f", "/home/arguggi/projects/heed/confs/db/tables.sql"]
+        connect tempDB $ runSQLTests "SQL file should match haskell"
+
+runSQLTests :: String -> PG.Connection -> IO ()
+runSQLTests desc conn = hspec $
+    describe desc $
         forM_ tables $ \(trans, message) ->
             it message $ runTest conn (execQuery trans) >>= (`shouldSatisfy` (\x -> head x >= 0))
+
