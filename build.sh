c907af51099ca73a5d1881172949114a79fcb51b
Arguggi <git@arguggi.co.uk>
2017-02-22 12:57:31 +0100

    build.sh use cabal-head

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ VERSION=0.2.0.0;
 
 # Build the backend
 echo "Building"
-cabal new-build heed-backend
+cabal-head new-build heed-backend
 
 # Copy backend to to /usr/bin
 echo "Copying binary to /usr/bin/"
-sudo find dist-newstyle/build/heed-backend-$VERSION/ -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
+#sudo find dist-newstyle/build/heed-backend-$VERSION/ -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
+sudo find dist-newstyle/build/x86_64-linux/ghc-8.0.2/heed-backend-$VERSION/ -iname "heed-backend" -type f -exec cp {} /usr/bin/heed-backend \;
